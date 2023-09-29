93c6cc1a098928d25852acd0730d00c02244f53a
Jalexis Herrera <jalexis.herrera@udea.edu.co>
2023-09-29 13:35:35 -0500

    Personal Information Component

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,17 +1,19 @@
 import { ExtraSkills } from "../ExtraSkills";
 import { LanguageSkills } from "../LanguageSkills";
+import { PersonalInfo } from "../PersonalInfo";
 import { ProgrammingLanguages } from "../ProgrammingLanguages";
 
 const Sidebar = () => {
   return (
-    <aside className="w-[350px] p-2 flex flex-col gap-2 debug min-h-full justify-evenly">
-      <div className="container-card flex flex-col gap-5 px-6 py-4 h-full debug">
-        <div className="mx-1">imagen de perfil</div>
-        <div>resumen de info personal</div>
+    <aside className="w-80 px-3 flex flex-col debug min-h-full justify-evenly">
+      <div className="container-card flex flex-col gap-5 px-3 py-4 h-full debug">
+        <div className="mx-1">
+          <PersonalInfo />
+        </div>
       </div>
 
-      <div className="flex flex-col gap-4 px-2 py-4 h-full debug">
-        <h2 className="font-bold text-lg">Mis Habilidades</h2>
+      <div className="flex flex-col gap-4 px-3 py-4 h-full debug">
+        <h2 className="font-bold text-lg">My Skills</h2>
         <div>
           <LanguageSkills />
         </div>
