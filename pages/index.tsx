aa0bda87a62b3c7a2289dd51d144f28dfbed164d
Jalexis Herrera <jalexis.herrera@udea.edu.co>
2023-09-29 16:22:30 -0500

    I add the head to the index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Sidebar } from '../components/layout/Sidebar';
-import { CenterContent } from '../components/layout/CenterContent';
-import { RightMenu } from '../components/layout/RightMenu';
+import Image from "next/image";
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import { Sidebar } from "../components/layout/Sidebar";
+import { CenterContent } from "../components/layout/CenterContent";
+import { RightMenu } from "../components/layout/RightMenu";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex debug">
-      <Sidebar />
-      <CenterContent />
-      <RightMenu />
+    <div>
+      <Head>
+        <title>CV Jaime A. Herrer R.</title>
+        <meta property="og:title" content="CV Jaime A. Herrer R." key="title" />
+      </Head>
+      <div className="min-h-screen flex debug">
+        <Sidebar />
+        <CenterContent />
+        <RightMenu />
+      </div>
     </div>
   );
 };
- export default Home;
+export default Home;
