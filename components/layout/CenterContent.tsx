edeb8c53339854e26baec80435b54bc88871cf83
Jalexis Herrera <jalexis.herrera@udea.edu.co>
2023-10-07 17:23:57 -0500

    Create about-me component

--- components/layout/CenterContent.tsx
+++ components/layout/CenterContent.tsx
@@ -1,16 +1,19 @@
+import { AboutMe } from "../AboutMe";
 import { Footer } from "./Footer";
 const CenterContent = () => {
-    return (
-        <div className="flex flex-col justify-between gap-2 w-full">
-            <div className="debug">Slider princial con foto y texto</div>
-             <div className="debug">My Knowledge</div>
-             <div className="debug">Education</div>
-             <div className="debug">Porfolio</div>
-            <div className="justify-items-end"><Footer /></div>
-            
-        </div>
-
-    );
+  return (
+    <div className="flex flex-col gap-10 w-full justify-stretch min-h-screen">
+      <section className="debug">
+        <AboutMe />
+      </section>
+      <div className="debug">My Knowledge</div>
+      <div className="debug">Education</div>
+      <div className="debug">Porfolio</div>
+      <div className="justify-items-end">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export { CenterContent };
