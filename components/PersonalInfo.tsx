48fcfa17a1d480ee5878898c684c65907faeaeaf
Jalexis Herrera <jalexis.herrera@udea.edu.co>
2023-10-07 17:21:41 -0500

    minor changes

--- components/PersonalInfo.tsx
+++ components/PersonalInfo.tsx
@@ -1,49 +1,52 @@
 import Image from "next/image";
 import React from "react";
+import Tooltip from "@mui/material/Tooltip";
 
 const PersonalInfo: React.FC = () => {
   return (
-    <div className="w-[150px] flex items-center flex-col mx-auto debug">
-    <div className="relative flex flex-col h-fit mx-3 mt-[50px] text-center items-center">
-       <div className="h-4 w-4 bg-green-500 rounded-full absolute bottom-28 right-4 border-1 border-white dark:border-gray-800"></div>
-      <Image
-        className="rounded-full"
-        src="/img/profile.jpg"
-        alt="Perfil photo"
-        width={150}
-        height={150}
-      />
-      <div className="flex flex-col gap-1">
-        <h3 className="text-lg font-[550] mt-8">Alexis Herrera</h3>
-        <p className="text-[15px] leading-6 text-text-secondary whitespace-nowrap">
-        Systems engineer student
-        </p>
+    <div className="flex items-center flex-col mx-auto">
+      <div className="relative flex flex-col h-fit mx-3 mt-auto text-center items-center">
+        <Tooltip title="Disponible para trabajar" placement="right">
+          <div className="h-4 w-4 bg-green-500 rounded-full absolute bottom-20 right-4 border-1 border-white dark:border-gray-800"></div>
+        </Tooltip>
+        <Image
+          className="rounded-full"
+          src="/img/profile.jpg"
+          alt="Perfil photo Jaime A. Herrera R."
+          width={100}
+          height={100}
+        />
+        <div className="flex flex-col gap-auto">
+          <h3 className="text-md font-[550] mt-1">Alexis Herrera</h3>
+          <p className="text-sm leading-6 text-text-secondary whitespace-nowrap">
+            Systems Engineer Student
+          </p>
+        </div>
       </div>
-    </div>
-    {/*--------*/}
-    <div className="container">
-        <hr className="mx-1 my-10" />
+      {/*--------*/}
+      <div className="container">
+        <hr className="mx-1 my-3" />
       </div>
 
-      <div className="flex flex-col gap-2 min-w-full text-[15px]">
-        <div className="flex justify-between">
-          <h4>Age:</h4>
-          <h4>35</h4>
+      <div className="flex flex-col gap-2 min-w-full leading-none">
+        <div className="flex justify-between items-center">
+          <h4>Tel:</h4>
+          <p className="textSkills flex">+57 3176721091</p>
         </div>
-        <div className="flex justify-between">
-          <h4>Residence:</h4>
-          <h4>CO</h4>
+        <div className="flex justify-between items-center">
+          <h4>Email:</h4>
+          <p className="flex textSkills">alexizher@gmail.com</p>
         </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center">
           <h4>Freelance:</h4>
-          <h4 className="text-[#7EB942]">Available</h4>
+          <p className="flex textSkills text-[#1b6446]">Available</p>
         </div>
-        <div className="flex justify-between">
+        {/*<div className="flex justify-between">
           <h4>Address:</h4>
           <h4>Medellin, Ant</h4>
-        </div>
+  </div>*/}
       </div>
-      </div>
+    </div>
   );
 };
 
